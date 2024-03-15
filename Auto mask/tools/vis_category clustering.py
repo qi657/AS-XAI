5b6c7400a70df37d5f708d95fe0eeeb99b0c9380
Chq Sun <52816600+qi657@users.noreply.github.com>
2024-03-15 21:04:40 +0800

    Update vis_category clustering.py

--- Auto mask/tools/vis_category clustering.py
+++ Auto mask/tools/vis_category clustering.py
@@ -35,7 +35,6 @@ pistil_data = data[pistil_indices, 2].astype(float)
 petal_data = data[petal_indices, 2].astype(float)
 other_data = data[other_indices, 2].astype(float)
 
-# 计算每个类别的平均值
 if name == 'cat' or name == 'dog':
     eye_values = data[eye_indices, 2].astype(float)
     eye_values = np.sum(eye_values)
@@ -68,47 +67,41 @@ elif name == 'primrose' or name == 'black eye susan':
     petal_mean = np.round(petal_values / total_sum * 10, 1)
     other_mean = np.round(other_values / total_sum * 10, 1)
 
-# 创建画布和子图
 # fig, ax = plt.subplots()
 fig,ax = plt.subplots(figsize=(7, 3), dpi=300)
 
-# 定义自定义颜色，以及相应的透明度
-# 使用LinearSegmentedColormap创建自定义渐变颜色映射
 if name == 'cat' or name == 'dog':
-    colors_eye = [(0, '#F9DBDB'), (0.5, '#C14C81'), (1, '#C14C81')]  # 红色渐变
+    colors_eye = [(0, '#F9DBDB'), (0.5, '#C14C81'), (1, '#C14C81')]  
     cmap_eye = LinearSegmentedColormap.from_list('custom_red', colors_eye, N=256)
 
-    colors_ear = [(0, '#FBE7D2'), (0.5, '#C38530'), (1, '#C38530')]  # 黄色渐变
+    colors_ear = [(0, '#FBE7D2'), (0.5, '#C38530'), (1, '#C38530')] 
     cmap_ear = LinearSegmentedColormap.from_list('custom_yellow', colors_ear, N=256)
 
-    colors_nose = [(0, '#D9DBF9'), (0.5, '#162D7D'), (1, '#162D7D')]  # 蓝色渐变
+    colors_nose = [(0, '#D9DBF9'), (0.5, '#162D7D'), (1, '#162D7D')]  
     cmap_nose = LinearSegmentedColormap.from_list('custom_blue', colors_nose, N=256)
 
-    colors_leg = [(0, '#D2F0EE'), (0.5, '#135E58'), (1, '#135E58')]  # 绿色渐变
+    colors_leg = [(0, '#D2F0EE'), (0.5, '#135E58'), (1, '#135E58')] 
     cmap_leg = LinearSegmentedColormap.from_list('custom_green', colors_leg, N=256)
 
 elif name == 'primrose' or name == 'black eye susan':
-    colors_other = [(0, '#D9DBF9'), (0.5, '#162D7D'), (1, '#162D7D')]  # 蓝色渐变
+    colors_other = [(0, '#D9DBF9'), (0.5, '#162D7D'), (1, '#162D7D')]  
     cmap_other = LinearSegmentedColormap.from_list('custom_blue', colors_other, N=256)
 
-    colors_pistil = [(0, '#F9DBDB'), (0.5, '#C14C81'), (1, '#C14C81')]  # 红色渐变
+    colors_pistil = [(0, '#F9DBDB'), (0.5, '#C14C81'), (1, '#C14C81')]  
     cmap_pistil = LinearSegmentedColormap.from_list('custom_red', colors_pistil, N=256)
 
-    colors_petal = [(0, '#FBE7D2'), (0.5, '#C38530'), (1, '#C38530')]  # 黄色渐变
+    colors_petal = [(0, '#FBE7D2'), (0.5, '#C38530'), (1, '#C38530')] 
     cmap_petal = LinearSegmentedColormap.from_list('custom_yellow', colors_petal, N=256)
 
 
-# 创建画布和子图
 # fig, ax = plt.subplots()
 
 if name =='cat':
-    # 绘制散点图，并使用c参数指定颜色，根据值的大小渐变
     ax.scatter(leg_data, ['Leg'] * len(leg_data), marker='o', c=leg_data, cmap=cmap_leg, alpha=0.5,s=50)
     ax.scatter(nose_data, ['Nose'] * len(nose_data), marker='o', c=nose_data, cmap=cmap_nose, alpha=0.5,s=50)
     ax.scatter(ear_data, ['Ear'] * len(ear_data), marker='o', c=ear_data, cmap=cmap_ear, alpha=0.5,s=50)
     ax.scatter(eye_data, ['Eye'] * len(eye_data), marker='o', c=eye_data, cmap=cmap_eye, alpha=0.5,s=50)
 elif name == 'dog':
-    # 绘制散点图，并使用c参数指定颜色，根据值的大小渐变
     ax.scatter(leg_data, ['Leg'] * len(leg_data), marker='o', c=leg_data, cmap=cmap_leg, alpha=0.5,s=50)
     ax.scatter(ear_data, ['Ear'] * len(ear_data), marker='o', c=ear_data, cmap=cmap_ear, alpha=0.5,s=50)
     ax.scatter(nose_data, ['Nose'] * len(nose_data), marker='o', c=nose_data, cmap=cmap_nose, alpha=0.5,s=50)
@@ -137,13 +130,10 @@ ax.set_title(f'Filter rank of {name} data',font2)
 # plt.legend(loc='upper left',fontsize=8)
 # ax.legend(loc='center right', bbox_to_anchor=(1, 1.07))
 
-# 设置纵轴标签
 ax.set_ylabel('Categories', font2)
 
-# 设置横轴标签
 ax.set_xlabel('Rank', font2)
 
-# 设置横轴刻度间隔
 xticks_interval = 1
 xticks_positions = np.arange(0, 8, xticks_interval)
 plt.xticks(xticks_positions,fontname='Arial', fontsize=13)
@@ -153,10 +143,8 @@ plt.xticks(xticks_positions,fontname='Arial', fontsize=13)
 plt.ylim(-0.5, 2.5)  # flowers
 # plt.ylim(-0.5, 1.5)
 
-# 设置纵轴刻度标签，并调整字体
 plt.yticks(fontname='Arial', fontsize=13)
 
 plt.savefig(f'../lime_save_2kinds/filter_rank_{name}.tiff',bbox_inches='tight', dpi=300)
 
-# 显示图表
 plt.show()
